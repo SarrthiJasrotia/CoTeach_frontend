398bfffd1f447245b6b74a220b9e886ef0fb62eb
npsaunders <neilpsaunders@gmail.com>
2022-10-03 10:52:02 -0400

    set state and get data from database to update content

--- src/components/body.js
+++ src/components/body.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Index from "../pages/index";
 import Show from "../pages/show";
 
 function Body(props) {
+    //set state for content and setContent function
+    const [content, setContent] = useState(null);
+
+    // **This is for local dev testing; switch to Heroku when done**
+    const URL = "http://localhost:4000/content/";
+
+    // This function is used to get the data from the database. It will wait until it is completed and the set the state of the content with the returned data
+    const getContent = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setContent(data);
+    }
+
     return (
         <main>
             <Switch>

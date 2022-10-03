a4d5805ad0890117f8373a239d0d8d1b76e0b3a0
npsaunders <neilpsaunders@gmail.com>
2022-10-03 10:45:50 -0400

    WIP: create router

--- src/components/body.js
+++ src/components/body.js
@@ -1,18 +1,25 @@
-import Content from "./content";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/index";
+import Show from "../pages/show";
 
-function Body(props){
-    return(
-        <div className="Body">
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
-        </div>
-    )
+function Body(props) {
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index />
+                </Route>
+                <Route
+                    path="/content/:id"
+                    render={(rp) => (
+                        <Show
+                            {...rp}
+                        />
+                    )}
+                />
+            </Switch>
+        </main>
+    );
 }
 
 export default Body;

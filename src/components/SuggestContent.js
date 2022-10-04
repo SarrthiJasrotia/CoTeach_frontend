031901c985aeaaf91643da1f2a5651062b3ce1ac
npsaunders <neilpsaunders@gmail.com>
2022-10-04 12:16:15 -0400

    wip: add SuggestContent

---
+++ src/components/SuggestContent.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Content from './content';
+import { Link } from "react-router-dom";
+
+function SuggestContent(props) {
+
+  function rndVideoNum() {
+    return Math.floor(Math.random() * props.content.length)
+  }
+
+  const rndNum = rndVideoNum();
+  const rndNum2 = rndVideoNum();
+
+  return (
+    <>
+      <div key={props.content[rndNum]._id} className="lesson">
+        <Link to={`/content/${props.content[rndNum]._id}`}>
+          <h1>{props.content[rndNum].title}</h1></Link>
+        <h3>{props.content[rndNum].category}</h3>
+        <Content videoURL={props.content[rndNum].videoURL} id={props.content[rndNum]._id} />
+      </div >
+      <div key={props.content[rndNum2]._id} className="lesson">
+        <Link to={`/content/${props.content[rndNum2]._id}`}>
+          <h1>{props.content[rndNum2].title}</h1></Link>
+        <h3>{props.content[rndNum2].category}</h3>
+        <Content videoURL={props.content[rndNum2].videoURL} id={props.content[rndNum2]._id} />
+      </div >
+    </>
+  );
+}
+
+export default SuggestContent;

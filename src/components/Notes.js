2d66eda1c084db306fbf43c19544fef767894d35
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-05 11:06:04 -0400

    notes textarea added

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,35 +1,69 @@
-import React, { useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React, { useRef, useState } from 'react';
 
 function Notes() {
+
+    const [newNote, setNewNote] = useState({
+     note: "",
+    })
+
+    const URL = "http://localhost:4000/notes/"
+
+        // This function is used to get the data from the database. It will wait until it is completed and the set the state of the content with the returned data
+        const getNotes = async () => {
+            const response = await fetch(URL);
+            const data = await response.json();
+            setNewNote(data);
+        }
+
+     // User can create a new note
+     const createNote = async (note) => {
+        await fetch(URL, {
+            method: "Post",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(note)
+        })
+        getNotes();
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        createNote(newNote);
+        setNewNote({
+          note: "",
+        });
+    };
+
+    // handleChange function for form
+    const handleChange = (event) => {
+        setNewNote({ ...newNote, [event.target.name]: event.target.value });
+    };
+
+
+
   const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
-      console.log(editorRef.current.getContent());
+      console.log("New Notes:", editorRef.current.getContent());
     }
   };
   return (
     <>
-      <Editor
-        apiKey='nd0bjucpchvuoxc6l8tqm6eee5a7r8c9yp73lm5pwflof25q'
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-          ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <button onClick={log}>Log editor content</button>
+    <form onSubmit={handleSubmit}>
+        <textarea
+        type="text"
+        value={newNote.notes}
+        name="notes"
+        placeholder="Enter your notes here."
+        onChange={handleChange} >
+                
+
+
+        </textarea>
+      
+      <input type="submit" value="Create Note" />
+      </form>
     </>
   );
 }

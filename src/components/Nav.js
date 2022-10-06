987c1f9290a7a86af0cbaf6c3529beb175c6c52e
npsaunders <neilpsaunders@gmail.com>
2022-10-06 09:23:17 -0400

    wip: auth

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,39 +2,71 @@ import React from 'react'
 import { login, logout } from '../services/firebase';
 
 const Nav = (props) => {
+  console.log("showbody", props)
+
+  const loginBut = () => {
+    return (<>
+      <div
+        onClick={login}
+        style={{
+          cursor: 'pointer',
+          marginRight: 10,
+          color: 'white'
+        }} >
+        Login
+      </div>
+    </>)
+  }
+
+  const logoutBut = () => {
+    return (
+      <>
+        <div style={{ marginRight: '10px', color: 'white' }}>Welcome, {props.user.displayName}</div>
+        <img src={props.user.photoURL} alt={props.user.name} style={{ height: '25px', marginRight: '10px' }} />
+        <div
+          onClick={logout}
+          style={{
+            cursor: 'pointer',
+            marginRight: 10,
+            color: 'white'
+          }} >
+          Logout
+        </div>
+      </>)
+  }
+
   return (
     <div className='nav'>
       <a href="/">HOME</a>
       <a href="/content/new">New</a>
-      {
-        props.user ?
-          <>
-            <div style={{ marginRight: '10px', color: 'white' }}>Welcome, {props.user.displayName}</div>
-            <img src={props.user.photoURL} alt={props.user.name} style={{ height: '25px', marginRight: '10px' }} />
-            <div
-              onClick={logout}
-              style={{
-                cursor: 'pointer',
-                marginRight: 10,
-                color: 'white'
-              }} >
-              Logout
-            </div>
-          </>
-          :
+      {!props.user ?
+
+        <div
+          onClick={login}
+          style={{
+            cursor: 'pointer',
+            marginRight: 10,
+            color: 'white'
+          }} >
+          Login
+        </div>
+
+        :
+        <>
+          <div style={{ marginRight: '10px', color: 'white' }}>Welcome, {props.user.displayName}</div>
+          <img src={props.user.photoURL} alt={props.user.name} style={{ height: '25px', marginRight: '10px' }} />
           <div
-            onClick={login}
+            onClick={logout}
             style={{
               cursor: 'pointer',
               marginRight: 10,
               color: 'white'
             }} >
-            Login
+            Logout
           </div>
-      }
-
+        </>}
     </div >
   )
 }
 
-export default Nav
+export default Nav;
